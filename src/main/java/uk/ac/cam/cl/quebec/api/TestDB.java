51a9c374364737a31647790bcc4caf47aba330b6
Callum Ryan <callum@MacBook-Pro-9.local>
2017-01-30 22:26:47 +0000

    Prints input parameters

--- src/main/java/uk/ac/cam/cl/quebec/api/TestDB.java
+++ src/main/java/uk/ac/cam/cl/quebec/api/TestDB.java
@@ -2,12 +2,22 @@ package uk.ac.cam.cl.quebec.api;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-// uk.ac.cam.cl.quebec.api.TestDB::testDBHandler
-public class TestDB {
-    public String testDBHandler(String inputJSON, Context context) {
+import java.util.LinkedHashMap;
+
+// uk.ac.cam.cl.quebec.api.TestDB::handleRequest
+public class TestDB implements RequestHandler<LinkedHashMap<String, String>, String> {
+    @Override
+    public String handleRequest(LinkedHashMap<String, String> input, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("received : " + inputJSON);
-        return String.valueOf("Input was: " + inputJSON);
+        logger.log("Serving request, input : " + input);
+
+        StringBuilder builder = new StringBuilder();
+        for (String key : input.keySet()) {
+            builder.append("< ").append(key).append(", ").append(input.get(key)).append(" >\n");
+        }
+        
+        return String.valueOf("Input " + builder.toString());
     }
 }
