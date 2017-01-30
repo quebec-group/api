57ffe55cd75d2b73fc066a8220f57b793145f3fa
Callum Ryan <callum@MacBook-Pro-9.local>
2017-01-30 19:57:58 +0000

    Added files

--- src/main/java/uk/ac/cam/cl/quebec/api/TestDB.java
+++ src/main/java/uk/ac/cam/cl/quebec/api/TestDB.java
@@ -1,4 +1,13 @@
 package uk.ac.cam.cl.quebec.api;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+// uk.ac.cam.cl.quebec.api.TestDB::testDBHandler
 public class TestDB {
+    public String testDBHandler(String inputJSON, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("received : " + inputJSON);
+        return String.valueOf("Input was: " + inputJSON);
+    }
 }
