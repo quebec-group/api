b1f06f6354690ac0d3a608a689a6f3f38ee7dcaa
Callum Ryan <callum@MacBook-Pro-9.local>
2017-01-31 16:37:43 +0000

    Serves API Gateway correctly

--- src/main/java/uk/ac/cam/cl/quebec/api/TestDB.java
+++ src/main/java/uk/ac/cam/cl/quebec/api/TestDB.java
@@ -2,22 +2,54 @@ package uk.ac.cam.cl.quebec.api;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.neo4j.driver.v1.*;
 
+import java.io.*;
+
 // uk.ac.cam.cl.quebec.api.TestDB::handleRequest
-public class TestDB implements RequestHandler<Object, String> {
+public class TestDB implements RequestStreamHandler {
 
     private static String DB_PASS = System.getenv("grapheneDBPass");
 
+    JSONParser parser = new JSONParser();
 
     @Override
-    public String handleRequest(Object input, Context context) {
-        if (context != null) {
-            LambdaLogger logger = context.getLogger();
-            logger.log("Serving request, input : " + input);
+    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+        LambdaLogger logger = context.getLogger();
+        logger.log("Loading Java Lambda handler of ProxyWithStream");
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        JSONObject responseJson = new JSONObject();
+        String responseCode = "200";
+
+        try {
+            JSONObject event = (JSONObject) parser.parse(reader);
+            JSONObject responseBody = new JSONObject();
+            responseBody.put("message", getDatabaseQueryResult());
+            responseBody.put("input", event.toJSONString());
+
+            responseJson.put("statusCode", responseCode);
+            responseJson.put("headers", new JSONObject());
+            responseJson.put("body", responseBody.toString());
+
+        } catch (ParseException pex) {
+            responseJson.put("statusCode", "400");
+            responseJson.put("exception", pex);
         }
 
+        logger.log(responseJson.toJSONString());
+        OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8");
+        writer.write(responseJson.toJSONString());
+        writer.close();
+    }
+
+
+    public String getDatabaseQueryResult() {
         StringBuilder builder = new StringBuilder();
 
         //Test database connection
@@ -26,7 +58,7 @@ public class TestDB implements RequestHandler<Object, String> {
 
         Session session = driver.session();
 
-        StatementResult result = session.run("MATCH (you {name:\"Andy\"})-[:FRIENDS_WITH]->(yourFriends) RETURN yourFriends");
+        StatementResult result = session.run("MATCH (you {name:\"Callum\"})-[:FRIENDS_WITH]->(yourFriends) RETURN yourFriends");
 
         while (result.hasNext()) {
             Record record = result.next();
@@ -41,5 +73,6 @@ public class TestDB implements RequestHandler<Object, String> {
         driver.close();
 
         return String.valueOf( builder.toString());
+
     }
 }
