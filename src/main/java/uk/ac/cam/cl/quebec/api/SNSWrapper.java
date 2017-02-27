ad76fc445e325fa82a8ca4e4021608c5420208fe
Callum Ryan <cpr41@cam.ac.uk>
2017-02-27 17:16:32 +0000

    Beginning of SNS

---
+++ src/main/java/uk/ac/cam/cl/quebec/api/SNSWrapper.java
@@ -0,0 +1,22 @@
+package uk.ac.cam.cl.quebec.api;
+
+import com.amazonaws.services.sns.AmazonSNSAsync;
+import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder;
+
+public class SNSWrapper {
+    private AmazonSNSAsync sns;
+    private static String ALL_ARN = "arn:aws:sns:eu-west-1:926867918335:quebec_alldevices_MOBILEHUB_1062763500";
+
+    public SNSWrapper() {
+        sns = AmazonSNSAsyncClientBuilder.defaultClient();
+    }
+
+    public void notifyAddedToEvent(String arn, String eventID) {
+        // Need to be able to get ARN for user
+        sns.publishAsync(arn, "You've been added to an event!" + eventID);
+    }
+
+    public void notifyFollowed(String arn, String followerID) {
+        sns.publishAsync(arn, followerID + " Followed you!");
+    }
+}
